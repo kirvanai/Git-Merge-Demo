05e108dafbaee02df3ab7ef3b6ad4aada7e2bf0f
Irving Kirvan <kirvanirving@gmail.com>
2023-10-13 11:09:21 -0400

    Initial Commit

---
+++ Git Merge Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Git_Merge_Demo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, this is Irving!");
+        }
+    }
+}
